e8b4920d286edbc68ccece97abdce335929e14e3
Anatolii Sakhnik <sakhnik@gmail.com>
2012-01-15 11:22:48 +0200

    Added a scratch of unit test suite.

---
+++ test/file3.cc
@@ -0,0 +1,20 @@
+//
+// file3.cc
+//
+//     Created: 15.01.2012
+//      Author: A. Sakhnik
+//
+
+#define BOOST_TEST_MAIN
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_SUITE(File3Test)
+
+BOOST_AUTO_TEST_CASE(First)
+{
+    BOOST_CHECK(true);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
+// vim: set et ts=4 sw=4:
